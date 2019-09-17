456a56ff58ca4278d709b505ffcc4094aed962c3
kobi.Medrish <kobi.medrish@gmail.com>
2019-09-17 23:29:57 +0300

    small fixes

--- stack/stack.h
+++ stack/stack.h
@@ -15,11 +15,11 @@
 typedef struct stack stack_t;
 
 /* 
- * Stack is an abstract data type that serves as a collection of elements, with
- * two principal operations: push, which adds an element to the collection, and
- * pop, which removes the most recently added element that was not yet removed.
- * The order in which elements come off a stack gives rise to its alternative
- * name, LIFO (last in, first out).
+ * Stack is an abstract data structure that serves as a collection of elements,
+ * with two principal operations: push, which adds an element to the collection,
+ * and pop, which removes the most recently added element that was not yet 
+ * removed. The order in which elements come off a stack gives rise to its 
+ * alternative name, LIFO (last in, first out).
  */
 
 
